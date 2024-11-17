10033c827824ba4232f691e8f5724b64ce26b06d
Ajaynirm <ajay@AJ-MacBook-Air-M1.local>
2024-11-17 17:34:33 +0530

    implemented hashing

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -3,17 +3,29 @@ import cors from 'cors';
 import startDBServer from '../database/connectDb.js';
 import t_Employee from '../model/employee.js'
 import t_login from '../model/login.js'
+import bodyParser from 'body-parser';
 import session from 'express-session'
+import cookieParser from 'cookie-parser'
+import bcrypt from 'bcrypt'
+import {Strategy} from 'passport-local'
 
 const app=express();
 
 app.use(cors());
 app.use(express.json()); 
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(cookieParser());
 app.use(session({
   secret:'hi',
   resave: false,
-  saveUninitialized: false,
+  saveUninitialized: true,
+  // cookie: { secure: false }, 
 }));
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+
 
 app.get('/', (req,res) => {
    req.session.isAuth = true;
@@ -26,7 +38,9 @@ app.post('/addAdmin', async (req,res) => {
  
 
   try{
-   const newAdmin = new t_login({t_userName,t_Pwd});
+    const hashedPassword = await bcrypt.hash(t_Pwd,10);
+   const newAdmin = new t_login({t_userName,t_Pwd:hashedPassword});
+   console.log(hashedPassword);
    await newAdmin.save();
    res.json({ success: true, message: 'Admin added successfully' });
   }catch(e){
@@ -39,19 +53,16 @@ app.post('/api/login', async (req, res) => {
    console.log(t_userName,t_Pwd);
 
     try {
-      console.log('up')
       const user = await t_login.findOne({ t_userName });
-      console.log('down')
       if (!user) {
         return res.status(400).json({ success: false, message: 'User  not found' });
       }
-      console.log("up2")
-      const isMatch = (t_Pwd === user.t_Pwd);
-      console.log("mid2")
+      
+      const isMatch = await bcrypt.compare(t_Pwd , user.t_Pwd);
+      console.log(user.t_Pwd);
       if (!isMatch) {
-        return res.status(400).json({ success: false, message: 'Invalid username or password' });
+        return res.status(400).json({ success: false, message: 'Invalid  password' });
       }
-      console.log("down2")
       res.json({ success: true, message: 'Login successful' });
   
     } catch (error) {
